0cd62f73b7366bc8930edf3f203ac72aac427b64
KoVLya <speedbreaker228@gmail.com>
2022-10-20 10:57:26 +0300

    laba 4 final

--- laba4/Main.java
+++ laba4/Main.java
@@ -1,4 +1,9 @@
 import java.util.*;
+import java.io.IOException;
+import java.io.StreamCorruptedException;
+import java.lang.Object;
+import java.util.regex.*;
+import java.util.regex.Pattern;
 
 /*
 algoritmics01000201000100aisd80998001000010001gov01010
@@ -8,10 +13,8 @@ qwertyuiopasdfghjklzxcvbnm23456789
 /*
 bab1001a102a3452a10000000000000001a10000000000001000a10001000100
 a
-1001
+001000010001
 */
-
-import java.util.regex.Pattern;
 
 public class Main{
 
@@ -32,9 +35,38 @@ public class Main{
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    public static int LengthOfString(String s){
+        Pattern pattern = Pattern.compile("[\\d]");
+        Matcher matches = pattern.matcher(s);
+        int lenght = 0;
+        while(matches.find()){
+            lenght++;
+        }
+        return lenght;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static boolean isNumeric(String s){
+        Pattern pattern = Pattern.compile("[\\d]");
+        Pattern pattern_0 = Pattern.compile("[\\D]");
+        int lenght_0 = 0, lenght_1 = 0;
+        Matcher matches = pattern.matcher(s);
+        while(matches.find()){
+            lenght_0++;
+        }
+        matches = pattern_0.matcher(s);
+        while(matches.find()){
+            lenght_1++;
+        }
+        return s != null && !Pattern.matches("[\\d]", s) && !Pattern.matches("[\\D]", s) && lenght_0 != 0 && lenght_1 != 0;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     public static boolean isNum(String s){
         try{
-            int num = Integer.parseInt(s, 2);
+            Integer.parseInt(s, 2);
 
         }catch(NumberFormatException ex){
             return false;
@@ -52,27 +84,27 @@ public class Main{
         String str_0 = scanner.nextLine();
         System.out.print("Enter string of separators: ");
         String str_1 = scanner.nextLine();
-        String[] words = new String[str_0.length()];
+        List<String> words = new ArrayList<>();
         System.out.print("Enter amount which you want to find: ");
         String P = scanner.nextLine();
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     if(str_1.length() == 1){
-        words = str_0.split(str_1);
+        String[] TempWords = new String[str_0.length()];
+        TempWords = str_0.split(str_1);
+        words = new ArrayList<>(Arrays.asList(TempWords));
     }
 
     else{
         StringTokenizer tokenizer = new StringTokenizer(str_0, str_1);
-        int i = 0; int max = 0;
-
+        int max = 0; int i = 0;
         while(tokenizer.hasMoreTokens()){
-            words[i] = tokenizer.nextToken();
-            if(words[i].length() > max){
-                maxString = words[i];
-                max = words[i].length();
+            words.add(tokenizer.nextToken());
+            if(words.get(i).length() > max){
+                maxString = words.get(i);
+                max = words.get(i).length();
             }
-            System.out.println(words[i]);
             i++;
         }
     }  //все числа в двоичной системе  
@@ -80,20 +112,20 @@ public class Main{
     System.out.println("---------------------------------------------------------------------------------------");
     System.out.println("All tokens: ");
     for(String word : words){
-        System.out.println(word);
+        System.out.print(String.format("%s ", word));
     }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    System.out.println("---------------------------------------------------------------------------------------");
+    System.out.println("\n---------------------------------------------------------------------------------------");
     System.out.println("All tokens that are numbers: ");
     List <String> arraylist_1 = new ArrayList<>();
+
     for(String word : words){
         if(isNum(word) == true){
             arraylist_1.add(word);
         }
-
-        System.out.println(word);
+        System.out.print(String.format("%s ", word));
     }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -105,7 +137,7 @@ public class Main{
         arraylist_1.get(i).getChars(0, arraylist_1.get(i).length(), str_2, 0);
 
         if(arraylist_1.get(i).compareTo(P) == 0){
-            System.out.println(str_0.indexOf(P)); //поиск первого вхождения строки
+            System.out.println(String.format("Found index first occurrence of string: %d", str_0.indexOf(P))); //поиск первого вхождения строки
         }
 
         for(int k = 0; k < arraylist_1.get(i).length(); k++){
@@ -116,7 +148,7 @@ public class Main{
         }
     }  // поиск палиндрома
 
-    System.out.println("---------------------------------------------------------------------------------------");
+    System.out.println("\n---------------------------------------------------------------------------------------");
     System.out.println("All numbers that are not palindrome: ");
     for(String pal : palindromesList){
         System.out.println(pal);
@@ -125,33 +157,37 @@ public class Main{
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     Collections.sort(arraylist_1, (s0, s1) -> Integer.valueOf(Integer.parseInt(s0, 2)).compareTo(Integer.valueOf(Integer.parseInt(s1, 2))));
-    List<String> FirstNumberAndSymbol = new ArrayList<>(Arrays.asList(words));
+    List<String> FirstNumberAndSymbol = words;
     StringBuffer buffer = new StringBuffer(str_0);
 
-    System.out.println("---------------------------------------------------------------------------------------");
-    System.out.println("First after deleting token consist numbers and characters: ");
-    System.out.println(buffer);
-
     for(String nas : FirstNumberAndSymbol){
-        if(isNum(nas) == false && nas.length() != 0){
+        if(isNumeric(nas) == true && nas.length() != 0){
+            System.out.println(String.format("First token consists numbers and characters: %s", nas));
             buffer.delete(str_0.indexOf(nas), str_0.indexOf(nas) + nas.length());
             break;
         }
     }
 
+    System.out.println(String.format("The largest number: %s", arraylist_1.get(arraylist_1.size() - 1)));
+    System.out.println("---------------------------------------------------------------------------------------");
+    System.out.println(String.format("First after deleting token consists numbers and characters: \n%s", buffer.toString()));
+
     buffer.insert(0, arraylist_1.get(arraylist_1.size() - 1));
 
     System.out.println("---------------------------------------------------------------------------------------");
-    System.out.println("First line after adding the largest number: ");
-
-    System.out.println(buffer);
+    System.out.println(String.format("First line after adding the largest number: \n%s", buffer.toString()));
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    // Pattern pattern = Pattern.compile("A.++a")
-    // Collections.sort(FirstNumberAndSymbol, (s0, s1) -> Integer.valueOf(s0.length()).compareTo(s1.length()));
-    // System.out.println(FirstNumberAndSymbol);
+    List <String> sortListByAmount = FirstNumberAndSymbol;
+    Collections.sort(sortListByAmount, (s0, s1) -> Integer.valueOf(LengthOfString(s0)).compareTo(LengthOfString(s1)));
 
+    System.out.println("---------------------------------------------------------------------------------------");
+    System.out.println("Sorting list by amount of numbers: ");
+    for(String list : sortListByAmount){
+        System.out.print(String.format("%s ", list));
+    }
+    
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     }
