40e896774f45e6f09bf8f4f321ff4402575235c1
KoVLya <speedbreaker228@gmail.com>
2022-11-19 23:52:47 +0300

    xml parser update

--- laba6/src/main/java/com/laba_6a/XMLParser/XMLParser.java
+++ laba6/src/main/java/com/laba_6a/XMLParser/XMLParser.java
@@ -1,5 +1,33 @@
 package com.laba_6a.XMLParser;
 
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class XMLParser {
+
+    public void Parse(String filePath){
+
+    try {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = builder.parse("kjsfaj");
+        
+        NodeList list = document.getDocumentElement().getElementsByTagName()
+
+    } catch (ParserConfigurationException e) {
+        e.printStackTrace();
+    } catch (SAXException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    }
+
     
 }
